7425212d1243b8e8f1c0eaf67c7749daf201ee72
Robert Hutton <rhutton@unimelb.edu.au>
2016-05-06 20:48:20 +1000

    Mostly working.  Map overlays still broken.

--- utils/install-osgeo.sh
+++ utils/install-osgeo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 # 
 # Installation script for Online WhatIf
 #
@@ -36,40 +37,37 @@
 #set -x
 initial_pwd="`pwd`"
 
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root" 1>&2
-	exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#	echo "This script must be run as root" 1>&2
+#	exit 1
+#fi
 
 echo Updating OS packages and installing basic dependencies
-apt-get update && apt-get dist-upgrade -y
-apt-get install -y couchdb pwgen
+sudo apt-get update && sudo apt-get dist-upgrade -y
+sudo apt-get install -y couchdb pwgen
 
 # Set all variables and passwords (you may update these to your liking)
-pg_user=whatif
-pg_pass=`pwgen -n 16 -N 1`
-PGPASSWORD=$pg_pass # PGPASSWORD is the variable that pg_dump and pg_restore look for
-database=whatif-development
-schema=wifdemo
-geoserver_download_url="http://nchc.dl.sourceforge.net/project/geoserver/GeoServer/2.8.3/geoserver-2.8.3-war.zip"
-geoserver_file_name="geoserver-2.8.3-war.zip" # must end in .zip
-geoserver_master_pw_initial=geoserver
-geoserver_master_pw=`pwgen -n 16 -N 1`
-workspace=whatif
-datastore=whatifStore
-hostname=`hostname`
-auth_user=envision
-auth_pass=`pwgen -n 16 -N 1`
-auth_db=envisiondb
-email_user=admin
-email_pass=`pwgen -n 16 -N 1`
+export pg_user=whatif
+export pg_pass=`pwgen -n 16 -N 1`
+export PGPASSWORD=$pg_pass # PGPASSWORD is the variable that pg_dump and pg_restore look for
+export database=whatif-development
+export schema=wifdemo
+export geoserver_master_pw=geoserver
+export workspace=whatif
+export datastore=whatifStore
+export hostname=`hostname`
+export auth_user=envision
+export auth_pass=`pwgen -n 16 -N 1`
+export auth_db=envisiondb
+export email_user=admin
+export email_pass=`pwgen -n 16 -N 1`
 
 # if we've run before, get the passwords from the config files
 if [ -f /etc/aurin/whatif-combined.properties ]
 then
 	echo pulling passwords from /etc/aurin/whatif-combined.properties
 	pg_pass=`perl -ne 'while(/wif.geo.db.password=(.*)/g){print "$1";}' /etc/aurin/whatif-combined.properties`
-	geoserver_master_pw=`perl -ne 'while(/geoserver.password=(.*)/g){print "$1";}' /etc/aurin/whatif-combined.properties`
+	PGPASSWORD=$pg_pass # PGPASSWORD is the variable that pg_dump and pg_restore look for
 fi
 if [ -f /etc/aurin/envision-combined.properties ]
 then
@@ -81,19 +79,19 @@ fi
 # whatif expects /usr/local/bin/pgsql2shp but Ubuntu has /usr/bin/pgsql2shp
 if [ ! -e /usr/local/bin/pgsql2shp ]
 then
-	( cd /usr/local/bin && ln -s /usr/bin/pgsql2shp )
+	( cd /usr/local/bin && sudo ln -s /usr/bin/pgsql2shp )
 fi
 
 # Update JAVA_OPTS
 if ! grep -q "^JAVA_OPTS.*/etc/aurin" /etc/default/tomcat7
 then
-	sed -i 's/^JAVA_OPTS=.*/JAVA_OPTS="-Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Dfile.encoding=UTF-8 -Xmx2G -XX:PermSize=512M -Daurin.dir=\/etc\/aurin"/' /etc/default/tomcat7
+	sudo sed -i 's/^JAVA_OPTS=.*/JAVA_OPTS="-Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Dfile.encoding=UTF-8 -Xmx2G -XX:PermSize=512M -Daurin.dir=\/etc\/aurin"/' /etc/default/tomcat7
 fi
 
 # Bind couchdb to all network interfaces
 if ! grep -q "^bind_address.*0.0.0.0" /etc/couchdb/default.ini
 then
-	sed -i 's/^bind_address.*/bind_address = 0.0.0.0/' /etc/couchdb/default.ini
+	sudo sed -i 's/^bind_address.*/bind_address = 0.0.0.0/' /etc/couchdb/default.ini
 fi
 
 # Create database
@@ -111,57 +109,41 @@ EOF
 fi
 
 # restore sample database.  Password is supplied in PGPASSWORD variable
-sudo pg_restore -Fc -U $pg_user -i -h localhost -p 5432 -d whatif-development $initial_pwd/../db/wanneroodump
-
-# Deploy geoserver on Tomcat
-if [ ! -e /var/lib/tomcat7/webapps/geoserver.war ]
+if ! sudo -u postgres psql << EOF | cut -d \| -f 1 |grep wifdemo > /dev/null
+\c whatif-development
+\dt *.*
+EOF
 then
-	UUIDGEN="`which uuidgen`"
-	if [ "$UUIDGEN" != "" ]
-	then
-		tempdir="/tmp/`uuidgen -t`"
-	else
-		tempdir="/tmp/6039708c-4551-11e5-9220-08002757beab"
-	fi
-	service tomcat7 stop
-	mkdir -p $tempdir && cd $tempdir
-	curl -s -o "$geoserver_file_name" "$geoserver_download_url"
-	geo_base="`basename $geoserver_file_name .zip`"
-	mkdir -p "$geo_base" && cd "$geo_base"
-	unzip -q "../$geoserver_file_name"
-	mv geoserver.war /var/lib/tomcat7/webapps/
-	chown root:root /var/lib/tomcat7/webapps/geoserver.war
-	cd "$initial_pwd"
-	rm -rf "$tempdir"
-	service tomcat7 start
+	pg_restore -Fc -U $pg_user -i -h localhost -p 5432 -d whatif-development $initial_pwd/../db/wanneroodump
 fi
 
-# update the master password
-if [ ! -f /etc/aurin/whatif-combined.properties ]
-then
-	xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<masterPassword>
-   <oldMasterPassword>$geoserver_master_pw_initial</oldMasterPassword>
-   <newMasterPassword>$geoserver_master_pw</newMasterPassword>
-</masterPassword>"
-	curl -v -u "admin:$geoserver_master_pw_initial" -XPUT -H "Content-type: text/xml" -d "$xml" http://localhost:8080/geoserver/rest/security/masterpw.xml
-	# update the admin password using the digest we just set for the master password (so the admin and master passwords will be the same).
-	master_digest=$(</var/lib/tomcat7/webapps/geoserver/data/security/masterpw.digest)
-	sed -i "s/^\s*<user.*name=\"admin\".*>/<user enabled=\"true\" name=\"admin\" password=\"$master_digest\"\/>/" /var/lib/tomcat7/webapps/geoserver/data/security/usergroup/default/users.xml
-fi
+# allow the system-provided geoserver to handle long urls
+sudo sed -i "s/<Set name=\"confidentialPort\">8443<\/Set>/<Set name=\"confidentialPort\">8443</Set>\n            <Set name=\"headerBufferSize\">64000</Set>/" /usr/local/lib/geoserver-2.8.2/etc/jetty.xml
+
+# (re)start system-provided geoserver
+echo "restarting system-provided geoserver"
+/usr/local/lib/geoserver-2.8.2/bin/shutdown.sh > /dev/null 2>&1
+/usr/local/lib/geoserver-2.8.2/bin/startup.sh > /dev/null 2>&1 &
+
+echo -n "waiting for geoserver to start"
+until curl -s "http://localhost:8082/geoserver/web/" |grep "<title>GeoServer: Welcome</title>"
+do 
+	sleep 2
+done
+echo "."
 
 # create a new workspace
-if ! curl -s -u "admin:$geoserver_master_pw" -XGET -H "Accept: text/xml" -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces.xml |grep -q "<name>$workspace</name>"
+if ! curl -s -u "admin:$geoserver_master_pw" -XGET -H "Accept: text/xml" -H "Content-type: text/xml" http://localhost:8082/geoserver/rest/workspaces.xml |grep -q "<name>$workspace</name>"
 then
 	xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <workspace>
   <name>$workspace</name>
 </workspace>"
-	curl -v -u "admin:$geoserver_master_pw" -XPOST -H "Content-type: text/xml" -d "$xml" http://localhost:8080/geoserver/rest/workspaces
+	curl -v -u "admin:$geoserver_master_pw" -XPOST -H "Content-type: text/xml" -d "$xml" http://localhost:8082/geoserver/rest/workspaces
 fi
 
 # create a new datastore
-if ! curl -v -u "admin:$geoserver_master_pw" -XGET -H "Accept: text/xml" -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores.xml |grep -q "<name>$datastore</name>"
+if ! curl -v -u "admin:$geoserver_master_pw" -XGET -H "Accept: text/xml" -H "Content-type: text/xml" http://localhost:8082/geoserver/rest/workspaces/$workspace/datastores.xml |grep -q "<name>$datastore</name>"
 then
 	xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <dataStore>
@@ -198,40 +180,40 @@ then
     <entry key=\"user\">$pg_user</entry>
   </connectionParameters>
 </dataStore>"
-	curl -v -u "admin:$geoserver_master_pw" -XPOST -H "Content-type: text/xml" -d "$xml" "http://localhost:8080/geoserver/rest/workspaces/$workspace/datastores"
+	curl -v -u "admin:$geoserver_master_pw" -XPOST -H "Content-type: text/xml" -d "$xml" "http://localhost:8082/geoserver/rest/workspaces/$workspace/datastores"
 fi
 
 # Configure the apache reverse proxy
-a2ensite default-ssl > /dev/null
-a2enmod proxy_ajp > /dev/null
-a2enmod proxy_http > /dev/null
-a2enmod ssl > /dev/null
-a2enmod rewrite > /dev/null
+sudo a2ensite default-ssl > /dev/null
+sudo a2enmod proxy_ajp > /dev/null
+sudo a2enmod proxy_http > /dev/null
+sudo a2enmod ssl > /dev/null
+sudo a2enmod rewrite > /dev/null
 
 # feels like sed might be the wrong tool for this job, though it works
 # 000-default
 if ! grep -q "ProxyPass /geoserver" /etc/apache2/sites-enabled/000-default.conf
 then
-	sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:80>\n\tServerName $hostname/" /etc/apache2/sites-enabled/000-default.conf
+	sudo sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:80>\n\tServerName $hostname/" /etc/apache2/sites-enabled/000-default.conf
 	conf="	ProxyPreserveHost On
 
 	RewriteEngine  On
 	RewriteRule     ^/$             https://$hostname/whatif/      [R]
 
-	ProxyPass /geoserver ajp://localhost:8009/geoserver
-	ProxyPassReverse /geoserver ajp://localhost:8009/geoserver
+	ProxyPass /geoserver http://localhost:8082/geoserver
+	ProxyPassReverse /geoserver http://localhost:8082/geoserver
 
 	# Allow long GET requests for what-if
 	LimitRequestLine 64000"
 	conf=$(echo "$conf" | sed -e 's/[]\/$*.^|[]/\\&/g') # encode config file for sed
 	conf=$(echo "$conf" | sed -e 's/$/\\/g') # escape newlines
-	sed -i "s/<\/VirtualHost>/$conf \n<\/VirtualHost>/" /etc/apache2/sites-enabled/000-default.conf
+	sudo sed -i "s/<\/VirtualHost>/$conf \n<\/VirtualHost>/" /etc/apache2/sites-enabled/000-default.conf
 fi
 
 # default-ssl
 if ! grep -q "ProxyPass /whatif/" /etc/apache2/sites-enabled/default-ssl.conf
 then
-	sed -i "s/<VirtualHost _default_:443>/<VirtualHost _default_:443>\n\t\tServerName $hostname/" /etc/apache2/sites-enabled/default-ssl.conf
+	sudo sed -i "s/<VirtualHost _default_:443>/<VirtualHost _default_:443>\n\t\tServerName $hostname/" /etc/apache2/sites-enabled/default-ssl.conf
 	conf="	# Allow long GET requests for what-if
 		LimitRequestLine 64000
 
@@ -249,31 +231,31 @@ then
 		ProxyPassReverse /aurin-wif/ ajp://localhost:8009/aurin-wif/
 
 		# HTTP proxy as AJP doesn't have sufficiently large GET request support
-		ProxyPass /geoserver http://localhost:8009/geoserver
-		ProxyPassReverse /geoserver http://localhost:8009/geoserver
+		ProxyPass /geoserver http://localhost:8082/geoserver
+		ProxyPassReverse /geoserver http://localhost:8082/geoserver
 
 		ProxyPass /workbenchauth ajp://localhost:8009/workbenchauth
 		ProxyPassReverse /workbenchauth ajp://localhost:8009/workbenchauth"
 	conf=$(echo "$conf" | sed -e 's/[]\/$*.^|[]/\\&/g') # encode config file for sed
 	conf=$(echo "$conf" | sed -e 's/$/\\/g') # escape newlines
-	sed -i "s/<\/VirtualHost>/${conf} \n\t<\/VirtualHost>/" /etc/apache2/sites-enabled/default-ssl.conf
+	sudo sed -i "s/<\/VirtualHost>/${conf} \n\t<\/VirtualHost>/" /etc/apache2/sites-enabled/default-ssl.conf
 fi
 
 # apache2.conf
 if ! grep -q "Servername" /etc/apache2/apache2.conf
 then
-	sed -i "1iServername ${hostname}" /etc/apache2/apache2.conf
+	sudo sed -i "1iServername ${hostname}" /etc/apache2/apache2.conf
 fi
 
 # Configure tomcat, enabling AJP connector
 # XXX need to check existing file first
-sed -i "s/<\!-- Define an AJP 1.3 Connector on port 8009 -->/<\!-- Define an AJP 1.3 Connector on port 8009 -->\n    <Connector port=\"8009\" protocol=\"AJP\/1.3\" redirectPort=\"8443\" \/>/" /etc/tomcat7/server.xml
+sudo sed -i "s/<\!-- Define an AJP 1.3 Connector on port 8009 -->/<\!-- Define an AJP 1.3 Connector on port 8009 -->\n    <Connector port=\"8009\" protocol=\"AJP\/1.3\" redirectPort=\"8443\" \/>/" /etc/tomcat7/server.xml
 
 # create whatif configuration file
 if [ ! -e /etc/aurin/whatif-combined.properties ]
 then
 	sudo mkdir -p /etc/aurin
-	cat > /etc/aurin/whatif-combined.properties << EOF
+	sudo bash -c "cat > /etc/aurin/whatif-combined.properties << EOF
 # wif.geo
 wif.geo.db.password=$pg_pass
 wif.geo.db.port=5432
@@ -311,8 +293,8 @@ wif.ui.endpoint=https://$hostname/aurin-wif/
 wif.ui.authURL=https://$hostname/workbenchauth/
 wif.ui.authpub_URL=https://$hostname/workbenchauth/
 wif.ui.useTrustingHttpClient=true
-wif.ui.trusted.hosts="$hostname","192.168.56.101","localhost","127.0.0.1"
-wif.ui.trusted.paths="/node","/geoinfo","/data_registration/datasets","/data_registration/dataset","/dataregistration/datasets","/datastore","/aurin-data-provider","/dataprovider","/mservices","/middleware","/R/call","/workflow-api","/ui","/aurin-wif","/what-if","/temp","/whatif"
+wif.ui.trusted.hosts=\"$hostname\",\"192.168.56.101\",\"localhost\",\"127.0.0.1\"
+wif.ui.trusted.paths=\"/node\",\"/geoinfo\",\"/data_registration/datasets\",\"/data_registration/dataset\",\"/dataregistration/datasets\",\"/datastore\",\"/aurin-data-provider\",\"/dataprovider\",\"/mservices\",\"/middleware\",\"/R/call\",\"/workflow-api\",\"/ui\",\"/aurin-wif\",\"/what-if\",\"/temp\",\"/whatif\"
 #other
 wif.config.productionLevel=false
 wif.config.testAllocationArea=true
@@ -325,34 +307,37 @@ wif.config.middlewareService=https://api-dev.aurin.org.au/middleware/
 aurin.data-store-service.uri=https://api-dev.aurin.org.au/data_store/
 # printing - deprecated but required for now
 wif.ui.mapprintURL=http://localhost:3000/print/
-EOF
+EOF"
 fi
 
 # Install the authentication system
 # XXX need to check for db presence first
-sudo -u postgres createuser -S -D -R $auth_user
-sudo -u postgres createdb -O $auth_user $auth_db
-sudo -u postgres psql -q $auth_db << EOF
+if ! sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -w "$auth_db" > /dev/null
+then
+	sudo -u postgres createuser -S -D -R $auth_user
+	sudo -u postgres createdb -O $auth_user $auth_db
+	sudo -u postgres psql -q $auth_db << EOF
 ALTER USER $auth_user WITH PASSWORD '$auth_pass';
 create extension postgis;
 \q
 EOF
-sudo -u postgres psql $auth_db -f ../db/structure.sql
+	sudo -u postgres psql $auth_db -f ../db/structure.sql
+fi
 
 # Set up SMTP daemon for sending email to new users
 if ! dpkg -s postfix |grep Status |grep -q installed
 then
-	debconf-set-selections <<< "postfix postfix/mailname string $hostname"
-	debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-	apt-get install -y postfix
+	sudo bash -c "debconf-set-selections <<< \"postfix postfix/mailname string $hostname\"
+	debconf-set-selections <<< \"postfix postfix/main_mailer_type string 'Internet Site'\""
+	sudo apt-get install -y postfix
 fi
 #XXX need to make it not prompt about self-signed cert
 if ! dpkg -s dovecot-core |grep Status |grep -q installed
 then
-	debconf-set-selections <<< "dovecot-core dovecot-core/create-ssl-cert boolean true"
-	debconf-set-selections <<< "dovecot-core dovecot-core/ssl-cert-name string $hostname"
-	debconf-set-selections <<< "ssl-cert make-ssl-cert/hostname string $hostname"
-	apt-get install -y dovecot-core
+	sudo bash -c "debconf-set-selections <<< \"dovecot-core dovecot-core/create-ssl-cert boolean true\"
+	debconf-set-selections <<< \"dovecot-core dovecot-core/ssl-cert-name string $hostname\"
+	debconf-set-selections <<< \"ssl-cert make-ssl-cert/hostname string $hostname\""
+	sudo apt-get install -y dovecot-core
 fi
 
 # Enable the submission service
@@ -367,7 +352,7 @@ then
   -o smtpd_reject_unlisted_sender=yes"
 	conf=$(echo "$conf" | sed -e 's/[]\/$*.^|[]/\\&/g') # encode config file for sed
 	conf=$(echo "$conf" | sed -e 's/$/\\/g') # escape newlines
-	sed -i "s/#submission/${conf} \n#submission/" /etc/postfix/master.cf
+	sudo sed -i "s/#submission/${conf} \n#submission/" /etc/postfix/master.cf
 fi
 
 #Set up dovecot to allow user authentication within postfix.
@@ -381,13 +366,13 @@ then
   }"
 	conf=$(echo "$conf" | sed -e 's/[]\/$*.^|[]/\\&/g') # encode config file for sed
 	conf=$(echo "$conf" | sed -e 's/$/\\/g') # escape newlines
-	sed -i "s/  # Auth process is run as this user./${conf} \n  # Auth process is run as this user./" /etc/dovecot/conf.d/10-master.conf
+	sudo sed -i "s/  # Auth process is run as this user./${conf} \n  # Auth process is run as this user./" /etc/dovecot/conf.d/10-master.conf
 fi
 
 # Set up auth
-sed -i "s/#disable_plaintext_auth = yes/disable_plaintext_auth = yes/" /etc/dovecot/conf.d/10-auth.conf
-sed -i "s/#auth_username_format = %Lu/auth_username_format = %n/" /etc/dovecot/conf.d/10-auth.conf
-sed -i "s/^auth_mechanisms.*/auth_mechanisms = plain/" /etc/dovecot/conf.d/10-auth.conf
+sudo sed -i "s/#disable_plaintext_auth = yes/disable_plaintext_auth = yes/" /etc/dovecot/conf.d/10-auth.conf
+sudo sed -i "s/#auth_username_format = %Lu/auth_username_format = %n/" /etc/dovecot/conf.d/10-auth.conf
+sudo sed -i "s/^auth_mechanisms.*/auth_mechanisms = plain/" /etc/dovecot/conf.d/10-auth.conf
 
 # Connect postfix and dovecot
 if ! grep -q "smtpd_sasl_auth_enable = yes" /etc/postfix/main.cf
@@ -401,22 +386,22 @@ smtpd_sasl_local_domain =
 broken_sasl_auth_clients = yes"
 	conf=$(echo "$conf" | sed -e 's/[]\/$*.^|[]/\\&/g') # encode config file for sed
 	conf=$(echo "$conf" | sed -e 's/$/\\/g') # escape newlines
-	sed -i "s/inet_protocols = all/inet_protocols = all\n${conf} /" /etc/postfix/main.cf
+	sudo sed -i "s/inet_protocols = all/inet_protocols = all\n${conf} /" /etc/postfix/main.cf
 fi
 
 # Create a linux user to send/receive email:
 if ! getent passwd $email_user 2>&1 >/dev/null
 then
-	adduser $email_user --gecos "User Administration" --disabled-password
-	chpasswd << END
+	sudo adduser $email_user --gecos "User Administration" --disabled-password
+	sudo bash -c "chpasswd << END
 $email_user:$email_pass
-END
+END"
 fi
 
 #Create config file for auth system
 if [ ! -e /etc/aurin/envision-combined.properties ]
 then
-	cat > /etc/aurin/envision-combined.properties << EOF
+	sudo bash -c "cat > /etc/aurin/envision-combined.properties << EOF
 #Properties
 env.ui.authURL=http://localhost/workbenchauth/
 env.ui.authpub_URL=https://localhost/workbenchauth/
@@ -449,13 +434,13 @@ env.mail.username=$email_user
 env.mail.password=$email_pass
 env.mail.from=$email_user@$hostname
 env.mail.url=https://$hostname/workbenchauth/
-EOF
+EOF"
 fi
 
 # Build and deploy the war files
 
 # Install the build dependencies
-apt-get -y install git tig maven default-jdk
+sudo apt-get -y install git tig maven default-jdk
 
 # Clone the source and build
 mkdir -p dependencies && cd dependencies
@@ -489,9 +474,13 @@ then
 fi
 
 # Deploy the war files
+cd "$initial_pwd"
 sudo cp dependencies/workbenchauth/target/workbenchauth-1.0.0.war /var/lib/tomcat7/webapps/workbenchauth.war
 sudo cp dependencies/online-whatif-ui/target/whatif-1.0.war /var/lib/tomcat7/webapps/whatif.war
 sudo cp ../target/aurin-wif-1.0.war /var/lib/tomcat7/webapps/aurin-wif.war
+
+# Ensure that the self-signed SSL certificate is trusted by java
+sudo "$initial_pwd"/refresh-java-keystore.sh
 
 # Restart relevant services
 sudo service dovecot restart
